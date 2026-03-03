41be9afe116c2aa19631453b84e4da6a9a602d3c
Dubinin Daniil <cloexec1@gmail.com>
2026-03-03 16:01:56 +0000

    Solve 01-gpu-puzzles

--- 01-gpu-puzzles-1/puzzle.cuh
+++ 01-gpu-puzzles-1/puzzle.cuh
@@ -1,17 +1,27 @@
 #pragma once
 
 __global__ void Map(const float* data, float* out) {
-    // YOUR CODE HERE
+    const size_t idx = threadIdx.x; 
+    out[idx] = data[idx] + 10;
 }
 
 __global__ void Zip(const float* left, const float* right, float* out) {
-    // YOUR CODE HERE
+    const size_t idx = threadIdx.x; 
+    out[idx] = left[idx] + right[idx];
 }
 
 __global__ void Guard(const float* data, float* out, size_t size) {
-    // YOUR CODE HERE
+    const size_t idx = threadIdx.x; 
+    if (idx >= size) {
+        return;
+    }
+    out[idx] = data[idx] + 10;
 }
 
 __global__ void Block(const float* data, float* out, float value, size_t size) {
-    // YOUR CODE HERE
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= size) {
+        return;
+    }
+    out[idx] = data[idx] + value;
 }
