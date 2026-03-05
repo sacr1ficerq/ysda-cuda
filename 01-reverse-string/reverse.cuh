601e472d4e6dbdb64187667ee13aba6ddebb34cf
Dubinin Daniil <cloexec1@gmail.com>
2026-03-05 19:08:47 +0000

    Solve 01-reverse-string

--- 01-reverse-string/reverse.cuh
+++ 01-reverse-string/reverse.cuh
@@ -1,9 +1,29 @@
 #pragma once
 
 #include <cstddef>
-
 #include <cuda_helpers.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+#define N_BLOCKS(N, M) ((N) + (M) - 1) / (M)
+
+__global__ void ReverseKernel(char* s, size_t n) {
+    const size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= n / 2) {
+        return;
+    }
+    // thrust::swap(s[idx], s[n - idx - 1]);
+    char temp = s[idx];
+    s[idx] = s[n - 1 - idx];
+    s[n - 1 - idx] = temp;
+}
 
 void ReverseDeviceStringInplace(char* str, size_t length) {
-    // YOUR CODE HERE
+    if (length == 0 || length == 1) {
+        return;
+    }
+    size_t n_threads = 1024;
+    size_t n_blocks = N_BLOCKS(length / 2, n_threads);
+    ReverseKernel<<<n_blocks, n_threads>>>(str, length);
+    cudaDeviceSynchronize();
 }
