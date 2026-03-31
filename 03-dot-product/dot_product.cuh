88b809028685fb367db8b5d991c0621a11029d4c
Dubinin Daniil <cloexec1@gmail.com>
2026-03-31 18:53:41 +0000

    Fix 03-dot-product
    This tree sum should speed things up

--- 03-dot-product/dot_product.cuh
+++ 03-dot-product/dot_product.cuh
@@ -6,31 +6,42 @@
 size_t EstimateDotProductWorkspaceSizeBytes(size_t num_elements) {
     size_t thread_size = 512;
     size_t block_size = (num_elements + thread_size - 1) / thread_size;
-    return block_size * sizeof(double);
+    return block_size * sizeof(float);
 }
 
 __global__ void BlockDotProductKernel(const float* lhs_device, const float* rhs_device,
-                                      size_t num_elements, double* workspace_device) {
+                                      size_t num_elements, float* workspace_device) {
     const size_t index = static_cast<size_t>(blockDim.x) * blockIdx.x + threadIdx.x;
+    __shared__ float shared_buffer[512];
 
-    __shared__ double shared_buffer[512];
+    if (index > num_elements) {
+        shared_buffer[threadIdx.x] = 0;
+        return;
+    }
 
     if (threadIdx.x == 0) {
         workspace_device[blockIdx.x] = 0;
     }
 
-    if (index < num_elements) {
-        shared_buffer[threadIdx.x] = static_cast<double>(lhs_device[index]) * rhs_device[index];
-    }
+    shared_buffer[threadIdx.x] = lhs_device[index] * rhs_device[index];
     __syncthreads();
 
-    // reduce inside each kernel and put inside workspace[blockIdx.x]
-    if (index < num_elements) {
-        atomicAdd(workspace_device + blockIdx.x, shared_buffer[threadIdx.x]);
+    // half the sum
+    size_t cnt = 1 << 8;  // 512 / 2
+    while (cnt != 0) {
+        if (threadIdx.x < cnt) {
+            shared_buffer[threadIdx.x] += shared_buffer[threadIdx.x + cnt];
+        }
+        cnt >>= 1;
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) {
+        workspace_device[blockIdx.x] = shared_buffer[0];
     }
 }
 
-__global__ void SumKernel(size_t num_elements, double* workspace_device, float* out_device) {
+__global__ void SumKernel(size_t num_elements, float* workspace_device, float* out_device) {
     const size_t index = static_cast<size_t>(blockDim.x) * blockIdx.x + threadIdx.x;
     if (index < num_elements) {
         atomicAdd(out_device, workspace_device[index]);
@@ -41,16 +52,16 @@ void DotProduct(const float* lhs_device, const float* rhs_device, size_t num_ele
                 float* workspace_device, float* out_device) {
     size_t thread_size = 512;
     size_t block_size = (num_elements + thread_size - 1) / thread_size;
-    double* workspace_device_double = reinterpret_cast<double*>(workspace_device);
 
     BlockDotProductKernel<<<block_size, thread_size>>>(lhs_device, rhs_device, num_elements,
-                                                       workspace_device_double);
+                                                       workspace_device);
     cudaDeviceSynchronize();
 
     num_elements = block_size;
     thread_size = 512;
     block_size = (num_elements + thread_size - 1) / thread_size;
 
-    SumKernel<<<block_size, thread_size>>>(num_elements, workspace_device_double, out_device);
+    SumKernel<<<block_size, thread_size>>>(num_elements, workspace_device, out_device);
     cudaDeviceSynchronize();
 }
+
