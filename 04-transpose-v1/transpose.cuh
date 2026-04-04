7c89273bd6bc9e2e0b649133ef53945b69a6ad78
Dubinin Daniil <cloexec1@gmail.com>
2026-04-04 17:06:57 +0000

    Add naive slow 04-transpose-v1

--- 04-transpose-v1/transpose.cuh
+++ 04-transpose-v1/transpose.cuh
@@ -2,7 +2,26 @@
 
 #include <cuda_helpers.h>
 
+__global__ void NaiveTransposeKernel(const __half* input, size_t input_stride, __half* output,
+                                     size_t output_stride, size_t num_rows, size_t num_cols) {
+    size_t x = static_cast<size_t>(blockDim.x) * blockIdx.x + threadIdx.x;
+    size_t y = static_cast<size_t>(blockDim.y) * blockIdx.y + threadIdx.y;
+
+    if (y <= num_rows - 1 && x <= num_cols - 1) {
+        size_t input_idx = y * input_stride + x;
+        size_t output_idx = x * output_stride + y;
+        output[output_idx] = input[input_idx];
+    }
+}
+
 void TransposeDevice(const __half* input_device, size_t input_stride, __half* output_device,
                      size_t output_stride, size_t num_rows, size_t num_cols) {
-    // YOUR CODE HERE
+    dim3 block(32, 32, 1);
+    size_t grid_width = (num_cols + 32 - 1) / 32;
+    size_t grid_height = (num_rows + 32 - 1) / 32;
+    dim3 grid(grid_width, grid_height);
+
+    NaiveTransposeKernel<<<grid, block>>>(input_device, input_stride, output_device, output_stride,
+                                          num_rows, num_cols);
+    cudaDeviceSynchronize();
 }
